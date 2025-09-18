c048c81ccf5ce45462c9923736dca032c3fd31d4
liukdv <134488633+liukdv@users.noreply.github.com>
2025-09-18 07:54:19 +0200

    Add files via upload

---
+++ keymap_qmk/liukdv/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define MASTER_LEFT                 // USB on left; left is master
+#define SPLIT_LAYER_STATE_ENABLE    // share layer_state to right half
+#define SPLIT_MODS_ENABLE           // (optional) share modifiers
+#define SPLIT_WPM_ENABLE            // (optional) share WPM
+// DO NOT put BOOTMAGIC_* here
