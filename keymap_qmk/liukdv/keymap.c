a776a0cd7876ca88f1986446b4b90bb42f885ccf
liukdv <134488633+liukdv@users.noreply.github.com>
2025-10-29 08:58:16 +0100

    kedoa

--- keymap_qmk/liukdv/keymap.c
+++ keymap_qmk/liukdv/keymap.c
@@ -11,38 +11,38 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* COLEMAK */
 [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-  KC_LSFT,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, DF(_QWERTY),KC_LBRC,KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LGUI, KC_DEL,  MO(_EXTEND), KC_SPC,   KC_BSPC, KC_ENT, KC_RALT, KC_LALT
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                           KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+  KC_LSFT,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                           KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   DF(_QWERTY), KC_LBRC,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_LGUI, KC_DEL,  MO(_EXTEND), KC_SPC,      KC_BSPC,   KC_ENT, KC_RALT, KC_LALT
 ),
 
 /* QWERTY */
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,
-  KC_LSFT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, DF(_COLEMAK), KC_LBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
-                       KC_LGUI, KC_DEL, MO(_EXTEND_QW), KC_SPC, KC_BSPC, KC_ENT, KC_RALT, KC_LALT
+  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                                 KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                 KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,   KC_MINS,
+  KC_LSFT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                 KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN,KC_QUOT,
+  KC_LCTL,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,        DF(_COLEMAK), KC_LBRC,   KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH,KC_RSFT,
+                       KC_LGUI, KC_DEL, MO(_EXTEND_QW),  KC_SPC,       KC_BSPC,   KC_ENT, KC_RALT, KC_LALT
 
 ),
 
 /* EXTEND (Navigation, editing, function keys, symbols) */
 [_EXTEND] = LAYOUT(
-  KC_F12, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TAB,  KC_ESC, C(S(KC_Z)), KC_WBAK, KC_WFWD, S(KC_TAB),               KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_PSCR,
-  KC_LSFT,  KC_LALT, KC_LCTL, KC_LSFT, KC_RGUI, KC_TAB,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_GRV,
-  KC_LCTL,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_ENT, _______, KC_RBRC,KC_MPRV, KC_VOLD, KC_MPLY, KC_VOLU, KC_MNXT, KC_BSLS,
-                        KC_LGUI, KC_DEL, _______,         KC_SPC, KC_BSPC,KC_ENT, KC_RALT, KC_LALT
+  KC_F12, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_TAB,  KC_ESC, C(S(KC_Z)), KC_WBAK, KC_WFWD, S(KC_TAB),                    KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_PSCR,
+  KC_LSFT,  KC_LALT, KC_LCTL, KC_LSFT, KC_RGUI, KC_TAB,                        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_GRV,
+  KC_LCTL,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_ENT,   _______,   KC_RBRC,  KC_TILDE, KC_VOLD, KC_MPLY, KC_VOLU, KC_BSLS, KC_RSFT,
+                        KC_LGUI, KC_DEL, _______,         KC_SPC,    KC_BSPC,  KC_ENT, KC_RALT, KC_LALT
 ),
 
 [_EXTEND_QW] = LAYOUT(
-  KC_F12, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TAB,  KC_ESC, C(S(KC_Z)), KC_WBAK, KC_WFWD, S(KC_TAB),               KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_PSCR,
-  KC_LSFT,  KC_LALT, KC_LCTL, KC_LSFT, KC_RGUI, KC_TAB,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_GRV,
-  KC_LCTL,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_ENT, _______, KC_RBRC, KC_MPRV, KC_VOLD, KC_MPLY, KC_VOLU, KC_MNXT, KC_BSLS,
-                       KC_LGUI, KC_DEL, _______,          KC_SPC, KC_BSPC,KC_ENT, KC_RALT, KC_LALT
+  KC_F12, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_TAB,  KC_ESC, C(S(KC_Z)), KC_WBAK, KC_WFWD, S(KC_TAB),                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_PSCR,
+  KC_LSFT,  KC_LALT, KC_LCTL, KC_LSFT, KC_RGUI, KC_TAB,                         KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_GRV,
+  KC_LCTL,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_ENT,     _______, KC_RBRC,   KC_TILDE, KC_VOLD, KC_MPLY, KC_VOLU, KC_BSLS, KC_RSFT,
+                       KC_LGUI, KC_DEL, _______,            KC_SPC,  KC_BSPC,   KC_ENT, KC_RALT, KC_LALT
 ),
 };
 
