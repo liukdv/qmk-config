a4bfa72a18282a7176c5b519c972a07e6fd003de
liukdv <luca95dv@gmail.com>
2026-08-23 15:00:22 +0200

    swapped f4

--- keyball61/keymap.c
+++ keyball61/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,     _______,      _______,             _______,  _______, _______,                                      _______,             _______, _______, _______, _______,   _______,
         _______,     KC_ESC ,      C(S(KC_Z)),          KC_LEFT,  KC_RGHT, KC_TILDE,                                     KC_ASTR,             KC_7,    KC_8,    KC_9,    KC_QUOT,   KC_ESC ,
         _______,     C(KC_A),      KC_BSPC,             KC_MINS,  KC_PLUS, KC_DQUO,                                      KC_0,                KC_4,    KC_5,    KC_6,    KC_EQL,    _______,
-        _______,     C(KC_Z),      C(KC_X),             C(KC_C),  C(KC_V), KC_COMM,     KC_GRV,       A(KC_F4),          KC_DOT,              KC_1,    KC_2,    KC_3,    KC_SLSH,   _______,
+        _______,     C(KC_Z),      C(KC_X),             C(KC_C),  C(KC_V), KC_COMM,     KC_GRV,        _______,          KC_DOT,              KC_1,    KC_2,    KC_3,    KC_SLSH,   _______,
         _______,     _______,      _______,             _______,  _______, _______,     _______,       _______,          _______,                                        _______,   _______
     ),
 
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,      KC_F1,        KC_F2,               KC_F3,    KC_F4,   KC_F5,                                        KC_F6,               KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,
         _______,     DF(_GAMING),  _______,             _______,  KC_PSCR, _______,                                      KC_F11,              KC_F7,   KC_F8,   KC_F9,   KC_F12,    _______,
         _______,     UG_TOGG,      UG_PREV,             UG_NEXT,  _______, _______,                                      KC_F10,              KC_F4,   KC_F5,   KC_F6,   _______,   _______,
-        _______,     CPI_D1K,      CPI_D100,            CPI_I100, CPI_I1K, _______,     _______,       _______,          _______,             KC_F1,   KC_F2,   KC_F3,   _______,   _______,
+        _______,     CPI_D1K,      CPI_D100,            CPI_I100, CPI_I1K, _______,     _______,       A(KC_F4),         _______,             KC_F1,   KC_F2,   KC_F3,   _______,   _______,
         _______,     _______,      _______,             _______,  _______, _______,     _______,       _______,          _______,                                        _______,   _______
     ),
 
