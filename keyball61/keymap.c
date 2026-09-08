e88a3bb21b78a67e9a11bf4b96960037de793a56
liukdv <luca95dv@gmail.com>
2026-09-08 11:10:47 +0200

    added ctrl w

--- keyball61/keymap.c
+++ keyball61/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,      KC_F1,        KC_F2,               KC_F3,    KC_F4,   KC_F5,                                        KC_F6,               KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,
         _______,     KC_ESC ,      UG_TOGG,             UG_PREV,  UG_NEXT, DF(_GAMING),                                  KC_F11,              KC_F7,   KC_F8,   KC_F9,   KC_F12,    _______,
         _______,     KC_LALT,      KC_LCTL,             KC_LSFT,  KC_RGUI, _______,                                      KC_F10,              KC_F4,   KC_F5,   KC_F6,   _______,   _______,
-        _______,     CPI_D1K,      CPI_D100,            CPI_I100, CPI_I1K, KC_PSCR,     _______,       A(KC_F4),         _______,             KC_F1,   KC_F2,   KC_F3,   _______,   _______,
+        _______,     CPI_D1K,      CPI_D100,            CPI_I100, CPI_I1K, KC_PSCR,     _______,       A(KC_F4),         C(KC_W),             KC_F1,   KC_F2,   KC_F3,   _______,   _______,
         _______,     _______,      _______,             _______,  _______, _______,     _______,       _______,          _______,                                        _______,   _______
     ),
 
--- lily58/keymap.c
+++ lily58/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,  KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
         _______, KC_ESC,  _______, _______, _______, _______,                      KC_F11, KC_F7, KC_F8, KC_F9, KC_F12, _______,
         _______, KC_LALT, KC_LCTL, KC_LSFT, KC_RGUI, _______,                      KC_F10, KC_F4, KC_F5, KC_F6, _______, _______,
-        _______, _______, _______, _______, _______, KC_PSCR, DF(_GAMING),   A(KC_F4), _______,KC_F1, KC_F2, KC_F3, _______, _______,
+        _______, _______, _______, _______, _______, KC_PSCR, DF(_GAMING),   A(KC_F4), C(KC_W),KC_F1, KC_F2, KC_F3, _______, _______,
                                   _______, _______, _______, _______,        _______, _______, _______, _______
     ),
 
